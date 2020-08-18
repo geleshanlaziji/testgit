170d8e61402e94c7ebb8318b219f4830e1f7867d
geleshanlaziji <277322480@qq.com>
2020-08-18 21:31:16 +0800

    更新2

---
+++ src/main/java/com/qx/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.qx.git;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello git");
+		System.out.println("更新1");
+		System.out.println("更新2");
+	}
+
+}
